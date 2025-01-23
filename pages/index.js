b3919cc8eb2540cd8b838badace79d7b1f70d75b
Hendrik Schmieding <hegik@me.com>
2025-01-23 14:40:15 +0100

    test 5 weiterleitung

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
-import Head from 'next/head'
-import Header from '@components/Header'
+import Head from 'next/head';
+import Header from '@components/Header';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Hegik's Server Map</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        {/* Iframe hier einfügen */}
+        <iframe src="http://gamec25.dsh.gg:25588" /> 
       </main>
-
-  
     </div>
-  )
+  );
 }
