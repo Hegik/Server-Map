20db719f566882494e10f70bf11069205ac98d97
Hendrik Schmieding <hegik@me.com>
2025-01-23 14:54:48 +0100

    test 6 weiterleitung

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Header from '@components/Header';
 
 export default function Home() {
+  const router = useRouter();
+
+  // Weiterleitung durchführen (z.B. nach 3 Sekunden)
+  setTimeout(() => {
+    router.push('https://gamec25.dsh.gg:25588'); 
+  }, 3000);
+
   return (
     <div className="container">
       <Head>
@@ -10,8 +18,7 @@ export default function Home() {
       </Head>
 
       <main>
-        {/* Iframe hier einfügen */}
-        <iframe src="http://gamec25.dsh.gg:25588" /> 
+        <Header title="Weiterleitung in 3 Sekunden..." /> 
       </main>
     </div>
   );
